85a2c5da5fb96d305a59305ca9ae029b33f36917
Luc <34852@ma-web.nl>
2023-09-26 14:03:29 +0200

    frontpage mapped out, start to js

--- js/main.js
+++ js/main.js
@@ -16,17 +16,87 @@ class GetData {
     }
 }
 
+class Renderer {
+    render(whereToRender, whatToRender) {
+        document.querySelector(whereToRender).appendChild(whatToRender);
+    }
+}
+
 
 
 class Main{
+    renderer
+    data
+    frontPage
+    constructor(renderer, data){
+        this.renderer = renderer;
+        this.data = data;
+        this.frontPage = new FrontPage(this.renderer);
+
+    }
 
 }
 
-class App{
-    api
+class FrontPage{
+    renderer;
+    frontpageSection
+    frontPageMain;
+    aside;
+
+    constructor(renderer){
+        this.renderer = renderer;
+        this.frontpageSection = document.createElement("section");
+        this.frontpageSection.classList.add("frontPage");
+        this.render();
+
+        this.aside = new Aside(this.frontpageSection, this.renderer);
+    }
+    render(){
+        this.renderer.render("body", this.frontpageSection)
+    }
+}
+
+
+class Aside{
+    ul;
+    logo;
+    AsideItem;
+    frontpageSection;
+    renderer
+    constructor(frontpageSection, renderer){
+        this.frontpageSection = frontpageSection;
+        this.renderer = renderer;
+        this.ul = document.createElement("ul");
+        this.ul.classList.add("frontPage__nav");
+    }
+    render(){
+        
+    }
+}
+
+class AsideItem{
     constructor(){
-        this.api = new GetData("./data/data.json")
-        this.api.getJson().then((data) => {});
+
+    }
+}
+
+class FrontPageMain{
+    constructor(){
+
+    }
+}
+
+
+class App{
+    api;
+    main;
+    renderer;
+    constructor(){
+        this.api = new GetData("./data/data.json");
+        this.renderer = new Renderer();
+        this.api.getJson().then((data) => {
+            this.main = new Main(this.renderer, data)
+        });
     }
 }
 
